f37a0d1911211b4cb1b3c0cfebccfc5c68274f99
Gyb <479276132@qq.com>
2022-09-15 19:32:39 +0800

    添加绑定限制

--- Assets/Samples/Input System/1.4.2/Rebinding UI/RebindActionUI.cs
+++ Assets/Samples/Input System/1.4.2/Rebinding UI/RebindActionUI.cs
@@ -267,6 +267,10 @@ namespace UnityEngine.InputSystem.Samples.RebindUI
 
             // Configure the rebind.
             m_RebindOperation = action.PerformInteractiveRebinding(bindingIndex)
+                .WithControlsExcluding("<Mouse>/leftButton")
+                .WithControlsExcluding("<Mouse>/rightButton")
+                .WithControlsExcluding("<Mouse>/press")
+                .WithControlsExcluding("<Pointer>/position")
                 .OnCancel(
                     operation =>
                     {
