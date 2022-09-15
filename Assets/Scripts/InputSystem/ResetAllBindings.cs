7782f5523ecb9f8a305f52d04490b091c94220eb
Gyb <479276132@qq.com>
2022-09-15 23:09:12 +0800

    添加重置所有按键功能

---
+++ Assets/Scripts/InputSystem/ResetAllBindings.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class ResetAllBindings : MonoBehaviour
+{
+    [SerializeField] private InputActionAsset inputActions;
+
+    public void ResetBindings()
+    {
+        foreach (InputActionMap map in inputActions.actionMaps)
+        {
+            map.RemoveAllBindingOverrides();
+        }
+    }
+}
