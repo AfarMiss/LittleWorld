3127b7e496cb904fb930d94e3f0c46a0dd33b0eb
Gyb <479276132@qq.com>
2022-09-21 21:02:08 +0800

    校正人物行走

--- Assets/Scripts/Animation/TestMovement.cs
+++ Assets/Scripts/Animation/TestMovement.cs
@@ -11,25 +11,25 @@ public class TestMovement : MonoBehaviour
     public bool isIdle;
     public bool isCarrying;
     public ToolEffect toolEffect;
-    public bool isUsingToolRight; 
-    public bool isUsingToolLeft; 
-    public bool isUsingToolUp; 
+    public bool isUsingToolRight;
+    public bool isUsingToolLeft;
+    public bool isUsingToolUp;
     public bool isUsingToolDown;
-    public bool isLiftingToolRight; 
-    public bool isLiftingToolLeft; 
-    public bool isLiftingToolUp; 
+    public bool isLiftingToolRight;
+    public bool isLiftingToolLeft;
+    public bool isLiftingToolUp;
     public bool isLiftingToolDown;
-    public bool isPickingRight; 
-    public bool isPickingLeft; 
-    public bool isPickingUp; 
+    public bool isPickingRight;
+    public bool isPickingLeft;
+    public bool isPickingUp;
     public bool isPickingDown;
-    public bool isSwingingToolRight; 
-    public bool isSwingingToolLeft; 
-    public bool isSwingingToolUp; 
+    public bool isSwingingToolRight;
+    public bool isSwingingToolLeft;
+    public bool isSwingingToolUp;
     public bool isSwingingToolDown;
-    public bool idleRight; 
-    public bool idleLeft; 
-    public bool idleUp; 
+    public bool idleRight;
+    public bool idleLeft;
+    public bool idleUp;
     public bool idleDown;
 
     public void Update()
@@ -40,5 +40,6 @@ public class TestMovement : MonoBehaviour
    isPickingRight, isPickingLeft, isPickingUp, isPickingDown,
    isSwingingToolRight, isSwingingToolLeft, isSwingingToolUp, isSwingingToolDown,
    idleRight, idleLeft, idleUp, idleDown);
+        Debug.Log($"inputX, inputY, isWalking, isRunning, isIdle:{inputX},{inputY},{isWalking},{isRunning},{ isIdle}");
     }
 }
--- Assets/Scripts/Farm/FarmGameController.cs
+++ Assets/Scripts/Farm/FarmGameController.cs
@@ -5,8 +5,66 @@ using static UnityEngine.InputSystem.InputAction;
 
 public class FarmGameController : MonoBehaviour
 {
-    [Range(3, 10)] public float walkSpeed = 3f;
-    [Range(11, 20)] public float runSpeed = 5f;
+    private float inputX;
+    private float inputY;
+    private bool isWalking;
+    private bool isRunning;
+    private bool isIdle;
+    private bool isCarrying;
+    private ToolEffect toolEffect;
+    private bool isUsingToolRight;
+    private bool isUsingToolLeft;
+    private bool isUsingToolUp;
+    private bool isUsingToolDown;
+    private bool isLiftingToolRight;
+    private bool isLiftingToolLeft;
+    private bool isLiftingToolUp;
+    private bool isLiftingToolDown;
+    private bool isPickingRight;
+    private bool isPickingLeft;
+    private bool isPickingUp;
+    private bool isPickingDown;
+    private bool isSwingingToolRight;
+    private bool isSwingingToolLeft;
+    private bool isSwingingToolUp;
+    private bool isSwingingToolDown;
+    private bool idleRight;
+    private bool idleLeft;
+    private bool idleUp;
+    private bool idleDown;
+
+    //public float inputX;
+    //public float inputY;
+    //public bool isWalking;
+    //public bool isRunning;
+    //public bool isIdle;
+    //public bool isCarrying;
+    //public ToolEffect toolEffect;
+    //public bool isUsingToolRight;
+    //public bool isUsingToolLeft;
+    //public bool isUsingToolUp;
+    //public bool isUsingToolDown;
+    //public bool isLiftingToolRight;
+    //public bool isLiftingToolLeft;
+    //public bool isLiftingToolUp;
+    //public bool isLiftingToolDown;
+    //public bool isPickingRight;
+    //public bool isPickingLeft;
+    //public bool isPickingUp;
+    //public bool isPickingDown;
+    //public bool isSwingingToolRight;
+    //public bool isSwingingToolLeft;
+    //public bool isSwingingToolUp;
+    //public bool isSwingingToolDown;
+    //public bool idleRight;
+    //public bool idleLeft;
+    //public bool idleUp;
+    //public bool idleDown;
+
+    private bool isPressWalking;
+
+    [Range(1, 2)] public float walkSpeed = 2f;
+    [Range(4, 5)] public float runSpeed = 4f;
 
     private Vector2 input;
 
@@ -15,10 +73,54 @@ public class FarmGameController : MonoBehaviour
         input = context.ReadValue<Vector2>();
     }
 
+    public void OnWalking(CallbackContext context)
+    {
+        if (context.started)
+        {
+            isPressWalking = true;
+        }
+        if (context.canceled)
+        {
+            isPressWalking = false;
+        }
+    }
+
     private void FixedUpdate()
     {
+        #region ²ÎÊýÐÞ¸Ä
         var localPlayer = GameObject.FindObjectOfType<FarmPlayer>();
         var rigidgBody = localPlayer.GetComponent<Rigidbody2D>();
-        rigidgBody.MovePosition(rigidgBody.position + runSpeed * Time.fixedDeltaTime * input);
+
+        inputX = input.x;
+        inputY = input.y;
+
+        isIdle = !(input.x != 0 || input.y != 0);
+        isWalking = (input.x != 0 || input.y != 0) && isPressWalking;
+        isRunning = (input.x != 0 || input.y != 0) && !isPressWalking;
+
+        if (isWalking)
+        {
+            rigidgBody.MovePosition(rigidgBody.position + walkSpeed * Time.fixedDeltaTime * input);
+        }
+        if (isRunning)
+        {
+            rigidgBody.MovePosition(rigidgBody.position + runSpeed * Time.fixedDeltaTime * input);
+        }
+        if (isIdle)
+        {
+        }
+        #endregion
+
+        UpdateAnimation();
+    }
+
+    private void UpdateAnimation()
+    {
+        EventHandler.CallMovementEvent(inputX, inputY, isWalking, isRunning, isIdle, isCarrying, toolEffect,
+    isUsingToolRight, isUsingToolLeft, isUsingToolUp, isUsingToolDown,
+   isLiftingToolRight, isLiftingToolLeft, isLiftingToolUp, isLiftingToolDown,
+   isPickingRight, isPickingLeft, isPickingUp, isPickingDown,
+   isSwingingToolRight, isSwingingToolLeft, isSwingingToolUp, isSwingingToolDown,
+   idleRight, idleLeft, idleUp, idleDown);
     }
 }
