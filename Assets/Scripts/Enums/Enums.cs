e5f52c55c90a3410ae88f8da68690fc8eb3325ab
Gyb <479276132@qq.com>
2022-09-26 00:07:27 +0800

    添加物品列表

--- Assets/Scripts/Enums/Enums.cs
+++ Assets/Scripts/Enums/Enums.cs
@@ -3,3 +3,27 @@ public enum ToolEffect
     none,
     watering
 }
+
+public enum Direction
+{
+    up,
+    down,
+    left,
+    right
+}
+
+public enum ItemType
+{
+    seed,
+    commodity,
+    hoeing_tool,
+    chopping_tool,
+    breaking_tool,
+    collection_tool,
+    watering_tool,
+    reaping_tool,
+    none,
+    count,
+    furniture,
+    reapable_scenary,
+}
