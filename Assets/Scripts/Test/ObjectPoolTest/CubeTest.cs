189c5a3bd2980e13292ab739a89625ef9f00375d
Gyb <479276132@qq.com>
2022-11-06 23:22:58 +0800

    测试ObjectPool对象池

---
+++ Assets/Scripts/Test/ObjectPoolTest/CubeTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeTest : MonoBehaviour
+{
+    public Action OnFinish;
+    public bool usingPool;
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Finish")
+        {
+            OnFinish?.Invoke();
+        }
+    }
+
+    public void Init(Action OnFinish, bool usingPool)
+    {
+        this.OnFinish = OnFinish;
+        this.usingPool = usingPool;
+    }
+}
---
+++ Assets/Scripts/Test/ObjectPoolTest/ObjectPoolTest.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Pool;
+
+public class ObjectPoolTest : MonoBehaviour
+{
+    public int generateNum;
+    public bool usePool = false;
+    public GameObject cube;
+    private ObjectPool<GameObject> pool;
+
+    private void Start()
+    {
+        pool = new ObjectPool<GameObject>(OnCreate, OnGet, OnRelease, OnDestoryGameObject, true, 5000, 10000);
+    }
+
+    private void FixedUpdate()
+    {
+        for (int i = 0; i < generateNum; i++)
+        {
+            if (usePool)
+            {
+                pool.Get();
+            }
+            else
+            {
+                var go = Instantiate(cube, UnityEngine.Random.insideUnitSphere, Quaternion.identity);
+                go.GetComponent<CubeTest>().Init(() => Destroy(go), false);
+            }
+        }
+    }
+
+    private void OnDestoryGameObject(GameObject obj)
+    {
+        Destroy(obj);
+    }
+
+    private void OnRelease(GameObject obj)
+    {
+        obj.SetActive(false);
+    }
+
+    private void OnGet(GameObject obj)
+    {
+        obj.SetActive(true);
+        obj.transform.position = UnityEngine.Random.insideUnitSphere;
+    }
+
+    private GameObject OnCreate()
+    {
+        var go = Instantiate(cube, UnityEngine.Random.insideUnitSphere, Quaternion.identity);
+        go.GetComponent<CubeTest>().Init(() => pool.Release(go), true);
+        return go;
+    }
+}
