19e7ca6e2799766c35372d89a6517adefa0e7ef9
Gyb <479276132@qq.com>
2023-01-17 12:49:47 +0800

    [add]创建map类，准备随机生成地图

---
+++ Assets/Scripts/LittleWorld/Map/Map.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace LittleWorld.Map
+{
+    public class Map
+    {
+        public Vector2Int MapSize;
+    }
+
+
+}
