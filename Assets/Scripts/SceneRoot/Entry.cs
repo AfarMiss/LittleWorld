3f2d36a0719122989ce03781d71f464dd9e4c186
Gyb <479276132@qq.com>
2023-01-23 17:31:49 +0800

    [fix]尝试正确加载游戏场景(目前与GlobalPathManager未初始化有关)

--- Assets/Scripts/SceneRoot/Entry.cs
+++ Assets/Scripts/SceneRoot/Entry.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace LittleWorld.EntryUtility
+namespace LittleWorld
 {
     public class Entry : MonoBehaviour
     {
