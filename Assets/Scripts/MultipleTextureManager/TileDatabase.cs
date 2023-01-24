a17d6e95ba861c9acb2b3d091eb7dc49e7bb992b
Gyb <479276132@qq.com>
2023-01-24 20:14:58 +0800

    [add]添加Tiles

---
+++ Assets/Scripts/MultipleTextureManager/TileDatabase.cs
@@ -0,0 +1,31 @@
+﻿using LittleWorld;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+namespace LittleWorld
+{
+    [CreateAssetMenu(fileName = "TileDatabase", menuName = "ScriptableObject/TileDatabase")]
+    public class TileDatabase : ScriptableObject
+    {
+        public List<TileDetail> textureDetailList;
+
+        public List<Tile> waterPlainDetailList;
+
+        public Tile Get(string terrainName)
+        {
+            var item = textureDetailList.Find(x => x.spriteName == terrainName);
+            if (item == null)
+            {
+                return null;
+            }
+            var itemTile = item.tile;
+            if (itemTile == null)
+            {
+                return null;
+            }
+            return itemTile;
+        }
+    }
+}
---
+++ Assets/Scripts/MultipleTextureManager/TileManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+namespace LittleWorld
+{
+    public class TileManager : MonoSingleton<TileManager>
+    {
+        [SerializeField]
+        private TileDatabase database;
+
+
+        public Tile GetTerrain(string terrainName)
+        {
+            return database.Get(terrainName);
+        }
+
+        public Tile[] GetBasicTerrain()
+        {
+            return database.waterPlainDetailList.ToArray();
+        }
+
+
+    }
+}
