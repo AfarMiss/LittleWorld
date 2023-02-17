d4ef5f191e5d983e86719d62ff31c73ab5799ee4
Gyb <479276132@qq.com>
2023-02-17 23:30:48 +0800

    [add]添加武器配置信息

---
+++ Assets/Scripts/LittleWorld/Nature/Weapon.cs
@@ -0,0 +1,25 @@
+﻿using LittleWorld.MapUtility;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace LittleWorld.Item
+{
+    public class Weapon : WorldObject
+    {
+        public WeaponInfo WeaponInfo;
+        public Weapon(int itemCode, Vector2Int gridPos, Map map = null) : base(itemCode, gridPos, map)
+        {
+            if (ObjectConfig.ObjectInfoDic.TryGetValue(itemCode, out var weaponInfo))
+            {
+                this.WeaponInfo = weaponInfo as WeaponInfo;
+                ItemName = weaponInfo.itemName;
+            }
+        }
+
+        public override Sprite GetSprite()
+        {
+            return WeaponInfo.itemSprites[0];
+        }
+    }
+}
