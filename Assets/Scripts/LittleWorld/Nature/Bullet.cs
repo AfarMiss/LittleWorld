b78e32a547d8d173c28ada2b57c34e32e964e376
Gyb <479276132@qq.com>
2023-03-05 23:58:11 +0800

    [fix]子弹射击目标点

--- Assets/Scripts/LittleWorld/Nature/Bullet.cs
+++ Assets/Scripts/LittleWorld/Nature/Bullet.cs
@@ -9,30 +9,29 @@ namespace LittleWorld.Item
         private Vector3 dir;
         private float speed;
         private Vector3 targetPos;
+        private float runtime = -1;
+        private float curRuntime = 0;
         private void FixedUpdate()
         {
+            curRuntime += Time.fixedDeltaTime;
             transform.position += Time.fixedDeltaTime * dir * speed;
+            if (curRuntime >= runtime)
+            {
+                curRuntime = 0;
+                runtime = -1;
+                ObjectPoolManager.Instance.Putback(PoolEnum.Bullet.ToString(), this.gameObject);
+                Debug.Log($"回收子弹:hash:{GetHashCode()},pos:{this.transform.position}");
+            }
         }
 
         public void Init(Vector3 targetPos, Vector3 originalPos, float speed)
         {
-            var dir = targetPos - originalPos;
+            this.targetPos = targetPos;
+            var disatance = targetPos - originalPos;
             this.speed = speed;
-            this.dir = dir.normalized;
-        }
-
-        private void Start()
-        {
-            StartCoroutine(DestroySelf());
-        }
-
-        IEnumerator DestroySelf()
-        {
-            if (Vector3.Distance(this.transform.position, targetPos) > 0.05f)
-            {
-                yield return null;
-            }
-            ObjectPoolManager.Instance.Putback(PoolEnum.Bullet.ToString(), this.gameObject);
+            this.dir = disatance.normalized;
+            this.runtime = disatance.magnitude / speed;
+            Debug.Log($"生成子弹{GetHashCode()}:speed:{speed},targetPos:{targetPos},originalPos:{originalPos},pos:{this.transform.position}");
         }
     }
 }
