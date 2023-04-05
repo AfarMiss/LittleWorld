1d3627694bb829e14e55460cb5ba61d839687268
Gyb <479276132@qq.com>
2023-04-05 20:08:22 +0800

    [add]添加贝塞尔绘制类

---
+++ Assets/Scripts/Bezier/BezierCurve.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+namespace BezierHelper
+{
+    public class BezierCurve : MonoBehaviour
+    {
+        public Transform startPoint;
+        public Transform endPoint;
+
+        private Vector3[] points = new Vector3[4];
+        //private Vector3 currentTargetPosition;
+        private List<Vector3> drawPoints;
+
+        private void Start()
+        {
+            drawPoints = new List<Vector3>();
+        }
+
+        private void OnDrawGizmos()
+        {
+            foreach (var item in points)
+            {
+                Gizmos.color = Color.blue;
+                Gizmos.DrawCube(item, Vector3.one);
+            }
+        }
+
+        private void Update()
+        {
+            UpdateLine();
+        }
+
+        private void UpdateLine()
+        {
+            drawPoints.Clear();
+            points[0] = startPoint.position;
+            points[1] = startPoint.position + Vector3.up * 5f;
+            points[2] = endPoint.position + Vector3.down * 5f;
+            points[3] = endPoint.position;
+
+            DrawCurve();
+            GetComponent<LineRenderer>().positionCount = drawPoints.Count;
+            GetComponent<LineRenderer>().SetPositions(drawPoints.ToArray());
+
+        }
+
+        private void DrawCurve()
+        {
+            // 绘制曲线
+            for (float t = 0f; t <= 1f; t += 0.03f)
+            {
+                drawPoints.Add(CalculateBezierPoint(t, points));
+            }
+        }
+
+        private Vector3 CalculateBezierPoint(float t, Vector3[] points)
+        {
+            int n = points.Length - 1;
+            Vector3 point = Vector3.zero;
+
+            for (int i = 0; i <= n; i++)
+            {
+                float factor = BinomialCoefficient(n, i) * Mathf.Pow(1 - t, n - i) * Mathf.Pow(t, i);
+                point += points[i] * factor;
+            }
+
+            return point;
+        }
+
+        private float BinomialCoefficient(int n, int k)
+        {
+            float result = 1;
+
+            for (int i = 1; i <= k; i++)
+            {
+                result *= (n - i + 1.0f) / i;
+            }
+
+            return result;
+        }
+    }
+
+}
---
+++ Assets/Scripts/Bezier/BezierDraw.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace BezierHelper
+{
+
+    public class BezierDraw : MonoBehaviour
+    {
+        public GameObject v0, v1, a0;
+        LineRenderer lineRenderer;
+        //Vector3 v0, v1;//顶点
+        //Vector3 a0;
+        float gap = 0.05f;//绘制的0-1的间隙 越小曲线越接近曲线，
+
+        // Update is called once per frame
+        void Update()
+        {
+            for (float i = 0; i < 1; i += gap)
+            {
+                Debug.DrawLine(po(i, v0, v1, a0), po(i + gap, v0, v1, a0), Color.red);
+                Debug.DrawLine(v0.transform.position, a0.transform.position, Color.green);
+                Debug.DrawLine(a0.transform.position, v1.transform.position, Color.green);
+            }
+        }
+
+        private Vector3 po(float t, GameObject v0, GameObject v1, GameObject a0)//根据当前时间t 返回路径  其中v0为起点 v1为终点 a为中间点 
+        {
+            Vector3 a;
+            a.x = t * t * (v1.transform.position.x - 2 * a0.transform.position.x + v0.transform.position.x) + v0.transform.position.x + 2 * t * (a0.transform.position.x - v0.transform.position.x);//公式为B(t)=(1-t)^2*v0+2*t*(1-t)*a0+t*t*v1 其中v0为起点 v1为终点 a为中间点 
+            a.y = t * t * (v1.transform.position.y - 2 * a0.transform.position.y + v0.transform.position.y) + v0.transform.position.y + 2 * t * (a0.transform.position.y - v0.transform.position.y);
+            a.z = t * t * (v1.transform.position.z - 2 * a0.transform.position.z + v0.transform.position.z) + v0.transform.position.z + 2 * t * (a0.transform.position.z - v0.transform.position.z);
+            return a;
+        }
+    }
+}
