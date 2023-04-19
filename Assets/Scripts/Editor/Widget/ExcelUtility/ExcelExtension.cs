bb844c459c5c605498e7d35983aa95a061abe7db
Gyb <479276132@qq.com>
2023-04-19 23:46:42 +0800

    [add]尝试加入ExcelDataReader库

---
+++ Assets/Scripts/Editor/Widget/ExcelUtility/ExcelExtension.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEditor;
+using UnityEngine;
+using System.Text;
+using ExcelDataReader;
+
+namespace ExcelUtility
+{
+    public class ExcelUtility
+    {
+        [MenuItem("Tools/快速Excel->XML")]
+        public static void AutoExcelToXML()
+        {
+            var filePath = "Assets\\Excels\\Ores.xlsx";
+            using (var stream = File.Open(filePath, FileMode.Open, FileAccess.Read))
+            {
+                // Auto-detect format, supports:
+                //  - Binary Excel files (2.0-2003 format; *.xls)
+                //  - OpenXml Excel files (2007 format; *.xlsx, *.xlsb)
+                using (var reader = ExcelReaderFactory.CreateReader(stream))
+                {
+                    // Choose one of either 1 or 2:
+
+                    // 1. Use the reader methods
+                    do
+                    {
+                        while (reader.Read())
+                        {
+                            // reader.GetDouble(0);
+                        }
+                    } while (reader.NextResult());
+
+                    // 2. Use the AsDataSet extension method
+                    var result = reader.AsDataSet();
+
+                    // The result of each spreadsheet is in result.Tables
+                }
+            }
+        }
+    }
+}
