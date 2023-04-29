0de1456291fda509e5a6ebda13ee8a1a3aa37dc8
Gyb <479276132@qq.com>
2023-04-30 01:27:18 +0800

    [modify]尝试Excel转xml，ExcelDataReader的丢弃仍存在问题，明天继续解决

--- Assets/Scripts/Editor/Widget/ExcelUtility/ExcelExtension.cs
+++ Assets/Scripts/Editor/Widget/ExcelUtility/ExcelExtension.cs
@@ -1,47 +1,36 @@
 ﻿using System.IO;
 using UnityEditor;
 using ExcelDataReader;
-using System.Data;
 
-namespace ExcelUtility
+namespace ExcelUtil
 {
     public class ExcelUtility
     {
-        public static DataSet ConvertExcelToXml(string filePath)
+        public static void ConvertExcelToXml(string filePath)
         {
-            DataSet result = null;
             using (var stream = File.Open(filePath, FileMode.Open, FileAccess.Read))
             {
-                // Auto-detect format, supports:
-                //  - Binary Excel files (2.0-2003 format; *.xls)
-                //  - OpenXml Excel files (2007 format; *.xlsx, *.xlsb)
-                using (var reader = ExcelReaderFactory.CreateReader(stream))
+                IExcelDataReader reader = ExcelReaderFactory.CreateReader(stream);
                 {
-                    // Choose one of either 1 or 2:
+                    //do
+                    //{
+                    //    while (reader.Read())
+                    //    {
+                    //    }
+                    //} while (reader.NextResult());
 
-                    // 1. Use the reader methods
-                    do
-                    {
-                        while (reader.Read())
-                        {
-                            // reader.GetDouble(0);
-                        }
-                    } while (reader.NextResult());
-
-                    // 2. Use the AsDataSet extension method
-                    result = reader.AsDataSet();
-
-                    // The result of each spreadsheet is in result.Tables
+                    //var result = reader.AsDataSet();
+                    //string xmlFileName = "excel_data.xml";
+                    //result.WriteXml(xmlFileName);
                 }
+                reader.Close();
             }
-            return result;
         }
 
         [MenuItem("Tools/快速Excel->Ores")]
         public static void AutoExcelToXML()
         {
-            var filePath = "Assets\\Excels\\Ores.xlsx";
-            ConvertExcelToXml(filePath);
+            ConvertExcelToXml("Assets\\Excels\\Ores.xlsx");
         }
     }
 }
---
+++ Assets/Scripts/Xml/ExcelConvertor.cs
@@ -0,0 +1,39 @@
+﻿using ExcelDataReader;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+namespace ExcelTool
+{
+    public class ExcelConvertor
+    {
+        public static void ConvertExcelToXml(string filePath)
+        {
+            using (var stream = File.Open(filePath, FileMode.Open, FileAccess.Read))
+            {
+                using (var reader = ExcelReaderFactory.CreateReader(stream))
+                {
+                    do
+                    {
+                        while (reader.Read())
+                        {
+                        }
+                    } while (reader.NextResult());
+
+                    var result = reader.AsDataSet();
+                    string xmlFileName = "excel_data.xml";
+                    result.WriteXml(xmlFileName);
+                }
+                //reader.Close();
+            }
+            Debug.Log("已完成转换!");
+        }
+
+        public static void AutoExcelToXML()
+        {
+            ConvertExcelToXml("Assets\\Excels\\Ores.xlsx");
+        }
+    }
+}
