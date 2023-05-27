1d1fa1dddb76a19f8c2891bda13af1f672082ee8
Gyb <479276132@qq.com>
2023-05-27 13:17:37 +0800

    添加ReGoap

---
+++ Assets/Scripts/ReGoap/Unity/FSMExample/Agents/Enemy.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public int maxHealth = 100;
+    public int curHealth = 0;
+    public bool isAlive => curHealth > 0;
+    void Start()
+    {
+        curHealth = maxHealth;
+    }
+
+    void Update()
+    {
+        if (!isAlive) return;
+        if (curHealth <= 0)
+        {
+            Debug.Log(this.GetInstanceID() + "isDead!");
+        }
+    }
+}
